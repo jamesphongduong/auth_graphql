4b0ad84f50ea08a9e7a2feb3420ef0dbac919d69
James <james.duong93@gmail.com>
2019-12-19 15:18:56 +1100

    #91

--- client/index.js
+++ client/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ApolloClient from "apollo-client";
+import ApolloClient, { createNetworkInterface } from "apollo-client";
 import { ApolloProvider } from "react-apollo";
 import { Router, hashHistory, Route, IndexRoute } from "react-router";
 
 import App from "./components/App";
 
+// config to tell apolloclient to attach cookies when making GraphQL requests to backend
+const networkInterface = createNetworkInterface({
+  uri: "/graphql",
+  opts: {
+    credentials: "same-origin"
+  }
+});
+
 const client = new ApolloClient({
+  networkInterface,
   dataIdFromObject: o => o.id
 });
 
